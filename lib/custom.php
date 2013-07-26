424d84df4cb94d01c99ef8421dfea33ad86ecd90
Szabó Gábor <szabogabi@gmail.com>
2013-07-26 15:56:54 +0200

    first commit

---
+++ lib/custom.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Custom functions
+ */
+
+add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
+/**
+ * Initialize the metabox class.
+ */
+function cmb_initialize_cmb_meta_boxes() {
+
+	if ( ! class_exists( 'cmb_Meta_Box' ) )
+		require_once 'cmb/init.php';
+
+}
