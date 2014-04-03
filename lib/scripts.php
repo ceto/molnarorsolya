2d04d8a57871670a181cc75c29be856e2df4d3e3
Gabor Szabo <szabogabi@gmail.com>
2014-04-03 22:55:00 +0200

    grunt files setting

--- lib/scripts.php
+++ lib/scripts.php
@@ -16,7 +16,7 @@
 function roots_scripts() {
   //wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
   //wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/assets/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
-  wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
@@ -32,9 +32,9 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_plugins', get_template_directory_uri() . '/assets/js/plugins.js', false, null, true);
-  wp_register_script('roots_main', get_template_directory_uri() . '/assets/js/main.js', false, null, true);
-  //wp_register_script('roots_allscripts', get_template_directory_uri() . '/assets/js/allscripts.min.js', false, null, true);
+  //wp_register_script('roots_plugins', get_template_directory_uri() . '/assets/js/plugins.js', false, null, true);
+  //wp_register_script('roots_main', get_template_directory_uri() . '/assets/js/main.js', false, null, true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, null, true);
 
   wp_register_script('gmap', '//maps.googleapis.com/maps/api/js?v=3.exp&#038;sensor=false', false, null, true);
   
@@ -42,8 +42,7 @@ function roots_scripts() {
   wp_enqueue_script('jquery');
   wp_enqueue_script('modernizr');
   wp_enqueue_script('gmap');
-  wp_enqueue_script('roots_plugins');
-  wp_enqueue_script('roots_main');
+  wp_enqueue_script('roots_scripts');
 
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
